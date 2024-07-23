1911a17a8cd21b766a85689dc6a06becea5a91b1
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-07-23 21:09:16 +0500

    learn the basics and python shell

---
+++ 01-basics/hello.py
@@ -0,0 +1,14 @@
+print("hi")
+def print_n (n):
+    print(n)
+
+
+def print_name (name):
+    print("Hello " + name + ", Welcome to Code With Hak buddy")
+
+
+hello_one = "Hello"
+hello_two = "Hola"
+hello_three = "Hemlo jee kiddaa"
+
+# printN()
---
+++ 01-basics/hello_import.py
@@ -0,0 +1,3 @@
+from hello import printN
+
+printN()
