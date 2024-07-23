122df3af6efde6925554da08c7434e84cce4e527
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-07-23 16:28:51 +0500

    reset the project

--- 01-basics/hello.py
+++
@@ -1,7 +0,0 @@
-print("Hello, World!")
-
-def print_my_name(name):
-    print(name)
-    print("My name is:",name)
-
-print_my_name("Huzair")
--- 01-basics/import.py
+++
@@ -1,3 +0,0 @@
-from hello import print_my_name
-
-print_my_name("Abdullah")
